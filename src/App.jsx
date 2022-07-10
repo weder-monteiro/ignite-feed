6c298066001a8acbcc276c8b0625196938603ad7
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-07-10 18:17:26 -0300

    Css Global

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
-import "./styles.css";
+import "./global.css";
 
 export function App() {
   return (
   <div>
     <Header />
-    
+
     <Post 
       author="Weder Monteiro" 
       content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At veritatis corporis quod sapiente, blanditiis voluptatibus nesciunt, quae molestias fugit nulla consectetur nemo, soluta sed culpa nisi ea beatae deserunt illo."
