fd4e65162d044073afe6acf5878bf245a2f133dc
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-07-10 17:35:29 -0300

    Propriedades

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,14 @@ import { Post } from "./Post";
 export function App() {
   return (
   <div>
-    <Post />
-    <Post />
-    <Post />
+    <Post 
+      author="Weder Monteiro" 
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At veritatis corporis quod sapiente, blanditiis voluptatibus nesciunt, quae molestias fugit nulla consectetur nemo, soluta sed culpa nisi ea beatae deserunt illo."
+    />
+    <Post 
+      author="Diana Silva" 
+      content="Lorem ipsum dolor sit amet, consectetur adipisicing elit. At veritatis corporis quod sapiente, blanditiis voluptatibus nesciunt, quae molestias fugit nulla consectetur nemo, soluta sed culpa nisi ea beatae deserunt illo."
+    />
   </div>
   )
 }
