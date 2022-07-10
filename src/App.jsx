cad11e61083bf03f1fd3a719055f7307c89aef7f
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-07-10 17:25:31 -0300

    Componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-  return (<h1>Hello World</h1>)
-}
+import { Post } from "./Post";
 
-export default App
+export function App() {
+  return (
+  <div>
+    <Post />
+    <Post />
+    <Post />
+  </div>
+  )
+}
