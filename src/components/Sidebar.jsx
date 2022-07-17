99527ac3eddb644915c94218e50ff8bd6a21d72f
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-07-17 15:26:47 -0300

    Finalizando Sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,18 +1,23 @@
 import styles from './Sidebar.module.css';
+import { PencilLine } from 'phosphor-react'
 
 export function Sidebar() {
     return(
         <aside className={styles.sidebar}>
             <img 
                 className={styles.cover} 
-                src="https://images.unsplash.com/photo-1573166953836-06864dc70a21?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MjB8fGRldmVsb3BlciUyMHdvbWFufGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=300&q=40" 
+                src="https://images.unsplash.com/photo-1522252234503-e356532cafd5?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60" 
             />
             <div className={styles.profile}>
+                <img 
+                    className={styles.avatar}
+                    src="https://avatars.githubusercontent.com/u/94122433?v=4" 
+                />
                 <strong>Nome do usuario</strong>
                 <span>Cargo do usuario</span>
                 <footer>
                     <a href="#">
-                        Editar seu perfil
+                        <PencilLine size={20}/> Editar seu perfil
                     </a>
                 </footer>
             </div>
