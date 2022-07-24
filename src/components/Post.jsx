a5b04b36f4442cb8543fbc9164228e85bf6863cc
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-07-24 18:17:18 -0300

    Formulario de comentarios

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -30,6 +30,15 @@ export function Post() {
                     <a href="">#rocketseat</a>
                 </p>
             </div>
+            <form className={styles.commentForm}>
+                <strong>Deixe seu feedback</strong>
+
+                <textarea placeholder="Deixe um comentario"/>
+
+                <footer>
+                    <button type="submit">Publicar</button>
+                </footer>
+            </form>
         </article>
     )
 }
