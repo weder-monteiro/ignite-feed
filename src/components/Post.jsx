96e8d85dc273f43a68bfa8ed0dbf50d25ebc0810
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-12-03 08:54:37 -0300

    Estado (useState)

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,8 +3,14 @@ import ptBR from 'date-fns/locale/pt-BR';
 import { Comment } from './Comment';
 import { Avatar }  from './Avatar'
 import styles from './Post.module.css';
+import { useState } from 'react';
 
 export function Post({ author, publishedAt, content }) {
+    const [comments, setComments] = useState([
+        1,
+        2
+    ]);
+
     const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'ás' HH:mm'h'", {
         locale: ptBR,
     });
@@ -13,6 +19,12 @@ export function Post({ author, publishedAt, content }) {
         locale: ptBR,
         addSuffix: true,
     });
+
+    function handleCreateNewComment(event) {
+        event.preventDefault();
+        
+        setComments([...comments, comments.length + 1]);
+    }
 
     return (
         <article className={styles.post}>
@@ -37,7 +49,7 @@ export function Post({ author, publishedAt, content }) {
                     }
                 })}
             </div>
-            <form className={styles.commentForm}>
+            <form onSubmit={handleCreateNewComment} className={styles.commentForm}>
                 <strong>Deixe seu feedback</strong>
 
                 <textarea placeholder="Deixe um comentario"/>
@@ -47,7 +59,9 @@ export function Post({ author, publishedAt, content }) {
                 </footer>
             </form>
             <div className={styles.commentList}>
-                <Comment />
+                {comments.map(comment => {
+                    return <Comment />
+                })}
             </div>
         </article>
     )
