71b269cb740d1f095c32b6d8e9350e3606e318d7
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-12-11 16:58:40 -0300

    Validando formulario

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -29,7 +29,12 @@ export function Post({ author, publishedAt, content }) {
     }
 
     function handleNewCommentChange(event) {
+        event.target.setCustomValidity('');
         setNewCommentText(event.target.value);
+    }
+
+    function handleNewCommentInvalid(event) {
+        event.target.setCustomValidity('Esse campo é obrigatorio');
     }
 
     function deleteComment(commentToDelete) {
@@ -39,6 +44,8 @@ export function Post({ author, publishedAt, content }) {
 
         setComments(commentsWithoutDeletedOne);
     }
+
+    const isNewCommentEmpty = newCommentText.length == 0;
 
     return (
         <article className={styles.post}>
@@ -71,10 +78,14 @@ export function Post({ author, publishedAt, content }) {
                     placeholder="Deixe um comentario"
                     value={newCommentText}
                     onChange={handleNewCommentChange}
+                    onInvalid={handleNewCommentInvalid}
+                    required
                 />
 
                 <footer>
-                    <button type="submit">Publicar</button>
+                    <button type="submit" disabled={isNewCommentEmpty}>
+                        Publicar
+                    </button>
                 </footer>
             </form>
             <div className={styles.commentList}>
