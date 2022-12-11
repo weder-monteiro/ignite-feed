25c4c6d62a9861f7e0c681faa2f5ad098357052b
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-12-11 16:14:18 -0300

    Comunicação entre componentes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,7 +2,12 @@ import { Trash, ThumbsUp } from 'phosphor-react';
 import { Avatar }  from './Avatar'
 import styles from './Comment.module.css';
 
-export function Comment({ content }) {
+export function Comment({ content, onDeleteComment }) {
+
+    function handleDeleteComment() {
+        onDeleteComment(content);
+    }
+
     return (
         <div className={styles.comment}>
             <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/94122433?v=4"/>
@@ -17,7 +22,7 @@ export function Comment({ content }) {
                                 Cerca de 1h atrás
                             </time>
                         </div>
-                        <button title='Deletar comentário'>
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
                             <Trash size={24}/>
                         </button>
                     </header>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -32,6 +32,10 @@ export function Post({ author, publishedAt, content }) {
         setNewCommentText(event.target.value);
     }
 
+    function deleteComment(comment) {
+        console.log(`Deletar comentário ${comment}`);
+    }
+
     return (
         <article className={styles.post}>
             <header>
@@ -71,7 +75,13 @@ export function Post({ author, publishedAt, content }) {
             </form>
             <div className={styles.commentList}>
                 {comments.map(comment => {
-                    return <Comment key={comment} content={comment}/>
+                    return (
+                        <Comment 
+                            key={comment} 
+                            content={comment} 
+                            onDeleteComment={deleteComment} 
+                        />
+                    )
                 })}
             </div>
         </article>
