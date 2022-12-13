22b2d76895ba8329edba9a9bf207bc51a66e0b8b
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-12-13 19:45:27 -0300

    Extensão de interfaces

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -23,7 +23,11 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
 
     return (
         <div className={styles.comment}>
-            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/94122433?v=4" alt=""/>
+            <Avatar 
+                hasBorder={false} 
+                src="https://avatars.githubusercontent.com/u/94122433?v=4" 
+                alt=""
+            />
 
             <div className={styles.commentBox}>
                 <div className={styles.commentContent}>
